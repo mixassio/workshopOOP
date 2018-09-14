ade28cf6a7ba2500663d9789d7b4f250df68252c
mixassio <mixail.bondarev@gmail.com>
2018-09-15 00:31:15 +0300

    done

--- src/half.js
+++ src/half.js
@@ -1,15 +1,15 @@
 // @flow
 import axios from 'axios';
-import convert from 'xml-js';
 
 
 export default async (ip) => {
-  const response = await axios(`http://ipgeobase.ru:7020/geo?ip=${ip}`);
-  const result = convert.xml2json(response.data, { compact: true, spaces: 4 });
-  const objRes = JSON.parse(result)['ip-answer']['ip'];
-  if (objRes.message) {
-    return new Error('Not found');
-  }
-  const objNew = Object.keys(objRes).filter(el => objRes[el]['_text']);
-  return objNew.reduce((acc, el) => ({ ...acc, [el]: objRes[el]['_text'] }), {});
+  const response = await axios(`http://ip-api.com/json/${ip}`);
+  const {
+    city,
+    country,
+    lat,
+    lon,
+  } = response.data;
+  const result = { city, country, lat, lon };
+  return JSON.stringify(result);
 };
