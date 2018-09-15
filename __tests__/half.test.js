1088d454f1eeb80f6c6ffd872c9577582309ecf3
mixassio <mixail.bondarev@gmail.com>
2018-09-15 10:20:39 +0300

    change str -> obj and add test

--- __tests__/half.test.js
+++ __tests__/half.test.js
@@ -1,8 +1,9 @@
 // @flow
 
-import half from '../src';
+import downloadInfo from '../src';
 
 test('half', async () => {
-  const response = half('91.77.226.130');
-  expect(response.status).toBe('200');
+  const trueAnswer = '{"city":"Moscow","country":"Russia","lat":55.7522,"lon":37.6156}';
+  const objAnswer = await downloadInfo('91.77.226.130');
+  expect(JSON.stringify(objAnswer)).toBe(trueAnswer);
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // @flow
+import axios from 'axios';
 
-import half from './half';
 
-export default half;
+export default async (ip) => {
+  const response = await axios(`http://ip-api.com/json/${ip}`);
+  const {
+    city,
+    country,
+    lat,
+    lon,
+  } = response.data;
+  const result = { city, country, lat, lon };
+  return result;
+};
